a02babbad99863b1ee23102e9c4c054dce42aa60
ro-lucchesi <rodrigo.lucchesi@oboticario.com.br>
2025-08-10 21:56:13 -0300

    Adicionando config do firebase em arquivo separado

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+            apiKey: "AIzaSyCqYhcPQ-4LoXEr9WDapXh24HEd6SOb8eM",
+            authDomain: "word-game-lucky.firebaseapp.com",
+            projectId: "word-game-lucky",
+            storageBucket: "word-game-lucky.firebasestorage.app",
+            messagingSenderId: "1056109338873",
+            appId: "1:1056109338873:web:3b1a9cb6bb27dd89b9c228",
+            measurementId: "G-B3RFSKW9HV"
+        };
